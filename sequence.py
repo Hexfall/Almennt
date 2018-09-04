da41b3f886561468deb0777f838dfd5aeec4ff85
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-09-04 15:20:42 +0000

    Fixed Sequence and added PigLatin

--- sequence.py
+++ sequence.py
@@ -1,5 +1,5 @@
 Seq = [1,2,3]
-n = int(input("Enter the length of the sequence: "))
+n = int(input("Enter the length of the sequence: ")) - 1
 for i in range(2, n):
     Seq.append(Seq[i-2] + Seq[i-1] + Seq[i])
 for i in Seq:
