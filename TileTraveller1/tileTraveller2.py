093159433dbaade9b3e7f38518b41532b546d976
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-09-18 14:01:42 +0000

    Refactoraði TileTraveller2

--- TileTraveller1/tileTraveller2.py
+++ TileTraveller1/tileTraveller2.py
@@ -10,46 +10,34 @@
 #    annars:
 #      prenta Invalid
 # prenta Sigur
-x = 1
-y = 1
-leidir = "N"
-print("You can travel: (N)orth.")
-while True:
-    x2 = x
-    y2 = y
-    inntak = input("Direction: ").upper()
-    for i in leidir:
+def nyjarLeidir (x, y):
+    if x == 1 and y == 1:
+        return "N"
+    elif x == 1 and y == 2:
+        return "NES"
+    elif x == 1 and y == 3:
+        return "ES"
+    elif x == 2 and y == 1:
+        return "N"
+    elif x == 2 and y == 2:
+        return "SW"
+    elif x == 2 and y == 3:
+        return "EW"
+    elif x == 3 and y == 1:
+        return ""
+    elif x == 3 and y == 2:
+        return "NS"
+    elif x == 3 and y == 3:
+        return "SW"
+
+def validAtt (inntak, leid):
+    for i in leid:
         if i == inntak:
-            if inntak == "N":
-                y += 1
-            elif inntak == "E":
-                x += 1
-            elif inntak == "S":
-                y -= 1
-            elif inntak == "W":
-                x -= 1
-            break
-    else:
-        print("Not a valid direction!")
-    if x2 != x or y2 != y:
-        if x == 1 and y == 1:
-            leidir = "N"
-        elif x == 1 and y == 2:
-            leidir = "NES"
-        elif x == 1 and y == 3:
-            leidir = "ES"
-        elif x == 2 and y == 1:
-            leidir = "N"
-        elif x == 2 and y == 2:
-            leidir = "SW"
-        elif x == 2 and y == 3:
-            leidir = "EW"
-        elif x == 3 and y == 1:
-            break
-        elif x == 3 and y == 2:
-            leidir = "NS"
-        elif x == 3 and y == 3:
-            leidir = "SW"
+            return True
+    return False
+
+def prentaAttir(leid):
+    if leid != "":
         print("You can travel: ", end="")
         for i in range(len(leidir)):
             if i != 0:
@@ -63,4 +51,24 @@ while True:
             elif leidir[i] == "W":
                 print("(W)est", end="")
         print(".")
+
+x = 1
+y = 1
+leidir = "N"
+print("You can travel: (N)orth.")
+while not (x == 3 and y == 1):
+    inntak = input("Direction: ").upper()
+    if validAtt(inntak, leidir):
+        if inntak == "N":
+            y += 1
+        elif inntak == "E":
+            x += 1
+        elif inntak == "S":
+            y -= 1
+        elif inntak == "W":
+            x -= 1
+        leidir = nyjarLeidir(x, y)
+        prentaAttir(leidir)
+    else:
+        print("Not a valid direction!")
 print("Victory!")
