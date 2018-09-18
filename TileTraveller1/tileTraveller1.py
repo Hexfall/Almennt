bf87cdcae754a520f39247801c04bb5b49bb47bc
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-09-18 12:56:44 +0000

    Gerði algorithmann í TileTraveller

--- TileTraveller1/tileTraveller1.py
+++ TileTraveller1/tileTraveller1.py
@@ -1,3 +1,12 @@
 # https://github.com/Hexfall/Almennt/tree/master/TileTraveller1
-# while True:
-# Lesa 
+# x = 1
+# y = 1
+# prenta You can travel: (N)orth.
+# while x != 3 and y != 3:
+#    strengur_inntak = input().lower()
+#    ef inntak er valid:
+#      færa character
+#      prenta mögulegar áttir
+#    annars:
+#      prenta Invalid
+# prenta Sigur
