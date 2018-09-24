8a1b85ce9012f638e0e988ff7495dec3554d2d19
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2018-09-24 22:54:28 +0000

    Auto-commit

--- 3n+1.py
+++ 3n+1.py
@@ -1,12 +1,15 @@
 while True:
-    n = int(input("Integer to check: "))
-    print(bin(n))
-    count = 0
-    while n != 1:
-        if n % 2 == 0:
-            n /= 2
-        else:
-            n = n * 3 + 1
-        print(bin(int(n)))
-        count += 1
-    print(count)
+    try:
+        n = int(input("Integer to check: "))
+        print(bin(n))
+        count = 0
+        while n != 1:
+            if n % 2 == 0:
+                n /= 2
+            else:
+                n = n * 3 + 1
+            print(bin(int(n)))
+            count += 1
+        print(count)
+    except:
+        break
