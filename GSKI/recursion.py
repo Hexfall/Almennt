588ce18ede889aaa188061b98a41f9f3c6996daf
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-01-24 21:22:04 +0000

    Auto-commit

--- GSKI/recursion.py
+++ GSKI/recursion.py
@@ -36,4 +36,33 @@ def ack(m, n):
         return ack(m - 1, 1)
     return ack(m - 1, ack(m, n - 1))
 
-print(ack(4, 4))
+def length(inp):
+    if inp == "":
+        return 0
+    return 1 + length(inp[1:])
+
+def binary_search(lis, find):
+    half = int(len(lis)/2)
+    if len(lis) == 0 or (len(lis) == 1 and lis[0] != find):
+        return False
+    elif lis[half] == find:
+        return True
+    elif lis[half] < find:
+        return binary_search(lis[half + 1:], find)
+    else:
+        return binary_search(lis[:half], find)
+
+def x_ish(word, x):
+    def is_in(word, letter):
+        if word == "":
+            return False
+        if word[0] == letter:
+            return True
+        return is_in(word[1:], letter)
+    if x == "":
+        return True
+    elif not is_in(word, x[0]):
+        return False
+    return x_ish(word, x[1:])
+
+print(x_ish("gagnaskipan", "da"))
