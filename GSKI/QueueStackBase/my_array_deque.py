7e32dde75c85128f65fe92e5f29ff51907a5b9c6
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-02-19 15:33:56 +0000

    Auto-commit

--- GSKI/QueueStackBase/my_array_deque.py
+++ GSKI/QueueStackBase/my_array_deque.py
@@ -1,3 +1,16 @@
 
 class ArrayDeque():
-    pass
+    def __init__(self):
+        self.__size = 0
+    def push_back(self, value):
+        pass
+    def push_front(self, value):
+        pass
+    def pop_back(self):
+        if self.get_size() == 0:
+            return None
+    def pop_front(self):
+        if self.get_size() == 0:
+            return None
+    def get_size(self):
+        return self.__size
--- GSKI/QueueStackBase/my_stack.py
+++ GSKI/QueueStackBase/my_stack.py
@@ -2,4 +2,14 @@ from my_array_deque import ArrayDeque
 from my_linked_list import LinkedList
 
 class Stack:
-    pass
+    def __init__(self, type = "array"):
+        if type == "array":
+            self.__array = ArrayDeque()
+        else:
+            self.__array = LinkedList()
+    def push(self, value):
+        self.__array.push_front(value)
+    def pop(self):
+        return self.__array.pop_front()
+    def get_size(self):
+        return self.__array.get_size()
