95b4349ae314a82da23effecfefb487513a0d104
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-02-20 21:01:40 +0000

    Auto-commit

--- GSKI/QueueStackBase/my_array_deque.py
+++ GSKI/QueueStackBase/my_array_deque.py
@@ -1,16 +1,66 @@
 
 class ArrayDeque():
     def __init__(self):
-        self.__size = 0
+        self.__size = 16
+        self.__arr = [0] * self.__size
+        self.__current = 0
+        self.__ordered = True
     def push_back(self, value):
-        pass
+        self.resize_eval()
+        self.__current += 1
+        self.set_at(value, self.get_size() - 1)
+        self.__ordered = False
     def push_front(self, value):
-        pass
+        self.insert(value, 0)
+    def insert(self, value, index):
+        if 0 <= index <= self.get_size():
+            self.resize_eval()
+            self.__current += 1
+            for i in range(self.get_size() - 1, index, -1):
+                self.set_at(self.get_at(i - 1), i)
+            self.set_at(value, index)
+            self.__ordered = False
+    def get_at(self, index):
+        if self.valid_index(index):
+            return self.__arr[index]
+    def set_at(self, value, index):
+        if self.valid_index(index):
+            self.__arr[index] = value
     def pop_back(self):
         if self.get_size() == 0:
             return None
+        returnVal = self.get_at(self.get_size() - 1)
+        self.__current -= 1
+        return returnVal
     def pop_front(self):
         if self.get_size() == 0:
             return None
+        returnVal = self.__arr[0]
+        self.remove_at(0)
+        return returnVal
+    def remove_at(self, index):
+        if self.valid_index(index):
+            for i in range(index, self.get_size() - 1):
+                self.set_at(self.get_at(i + 1), i)
+            self.set_at(0, self.get_size() - 1)
+            self.__current -= 1
     def get_size(self):
-        return self.__size
+        return self.__current
+    def resize(self):
+        self.__size += 16
+        new_arr = [0] * self.__size
+        for i in range(self.get_size()):
+            new_arr[i] = self.__arr[i]
+        self.__arr = new_arr
+    def resize_eval(self):
+        if self.get_size() == self.__size:
+            self.resize()
+    def valid_index(self, index):
+        if 0 <= index < self.get_size():
+            return True
+        return False
+    def __str__(self):
+        s = ""
+        for i in range(self.get_size()):
+            s += str(self.get_at(i)) + " "
+        return s
--- GSKI/QueueStackBase/my_linked_list.py
+++ GSKI/QueueStackBase/my_linked_list.py
@@ -1,11 +1,25 @@
+class Node():
+    def __init__(self, data = None, nxt = None):
+        self.data = data
+        self.next = nxt
 
 class LinkedList():
     def __init__(self):
+        self.__nodes = None
+        self.back_node = None
         self.__size = 0
     def push_back(self, value):
-        pass
+        if self.get_size() == 0:
+            self.push_front(value)
+        else:
+            self.back_node.next = Node(value)
+            self.back_node = self.back_node.next
+            self.__size += 1
     def push_front(self, value):
-        pass
+        self.__nodes = Node(value, self.__nodes)
+        if self.get_size() == 0:
+            self.back_node = self.__nodes
+        self.__size += 1
     def pop_back(self):
         if self.get_size() == 0:
             return None
@@ -14,3 +28,11 @@ class LinkedList():
             return None
     def get_size(self):
         return self.__size
+    def get_back_node(self):
+        def get_back(node):
+            if node.next == None:
+                return node
+            return get_back(node.next)
+        if self.get_size() == 0:
+            return None
+        return get_back(self.__nodes)
