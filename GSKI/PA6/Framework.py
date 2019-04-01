78953dab686afd0617430e6b7d01cdbe5ac27a5d
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-04-01 14:32:52 +0000

    Auto-commit

--- GSKI/PA6/Framework.py
+++ GSKI/PA6/Framework.py
@@ -51,6 +51,8 @@ class Framework():
     
     def remove_member(self, phone):
         self.members.pop(phone)
+        for sport in self.sports:
+            self.remove_membership(sport, phone)
     
     def change_member(self, phone, new_phone):
         self.members[new_phone] = self.members[phone]
@@ -64,5 +66,33 @@ class Framework():
             self.membership[sport].pop(self.membership[sport].index(phone))
         except:
             pass
+    
+    def members_by_name(self):
+        keys = self.members.keys()
+        member_list = [[key] + self.members[key] for key in keys]
+        member_list.sort(key=lambda x: x[1])
+        return "\n".join(["\t".join([str(i) for i in member]) for member in member_list])
 
-fr = Framework()
+    def members_by_phone(self):
+        keys = self.members.keys()
+        member_list = [[key] + self.members[key] for key in keys]
+        member_list.sort(key=lambda x: x[0])
+        return "\n".join(["\t".join([str(i) for i in member]) for member in member_list])
+
+    def members_by_email(self):
+        keys = self.members.keys()
+        member_list = [[key] + self.members[key] for key in keys]
+        member_list.sort(key=lambda x: x[2])
+        return "\n".join(["\t".join([str(i) for i in member]) for member in member_list])
+
+    def members_by_DoB(self):
+        keys = self.members.keys()
+        member_list = [[key] + self.members[key] for key in keys]
+        member_list.sort(key=lambda x: x[3])
+        return "\n".join(["\t".join([str(i) for i in member]) for member in member_list])
+        
+    def get_member(self, phone):
+        s = "\t".join([str(i) for i in [phone] + self.members[phone]])
+        s += "\nMembership:\n\t"
+        s += "\n\t".join([key for key in self.membership.keys() if phone in self.membership[key]])
+        return s
--- GSKI/PA6/UI.py
+++ GSKI/PA6/UI.py
@@ -3,10 +3,11 @@ from Framework import Framework
 from datetime import datetime
 
 clear = lambda: os.system('cls')
-MAIN_MENU = "\t1 - Manage Sports\n\t2 - Manage Members\n\tq - Exit program"
+MAIN_MENU = "\t1 - Manage Sports\n\t2 - Manage Members\n\ts - Save\n\tq - Exit program"
 MANAGE_SPORTS = "\t1 - Add Sport\n\t2 - Remove Sport\n\t3 - List of Sports\n\t4 - Manage Sport\n\tq - Exit to Main Menu"
 MANAGE_SPORT = "\t1 - Add Member\n\t2 - Remove Member\n\t3 - List of Members\n\tq - Exit to Manage Sports"
 MANAGE_MEMBERS = "\t1 - Add or Change Member\n\t2 - Remove Member\n\t3 - Member list\n\t4 - View Member\n\tq - Exit to Main Menu"
+ORDER = "\t1 - Order by Name\n\t2 - Order by Phone\n\t3 - Order by Email\n\t4 - Order by DoB\n\tq - Exit to Manage Members"
 
 class UI():
     def __init__(self):
@@ -26,6 +27,8 @@ class UI():
                 self.Manage_Sports()
             elif command == "2":
                 self.Manage_Members()
+            elif command.lower() == "s":
+                self.Fr.save()
         self.Fr.save()
 
     def Manage_Sports(self):
@@ -65,9 +68,13 @@ class UI():
             command = self.Menu("Main Menu > Manage Sports > {}".format(sport), MANAGE_SPORT)
             if command == "1":
                 self.Add_membership(sport)
+            elif command == "2":
+                self.Remove_membership(sport)
+            elif command == "3":
+                self.View_sport(sport)
             
     def Add_membership(self, sport):
-        phone = self.Menu("Main Menu > Manage Sports > {} > Add Membership".format(sport), "Phone to add: ")
+        phone = int(self.Menu("Main Menu > Manage Sports > {} > Add Membership".format(sport), "Phone to add: "))
         if phone not in self.Fr.members.keys():
             print("Unknown Member")
             input()
@@ -75,7 +82,7 @@ class UI():
         self.Fr.add_membership(sport, phone)
     
     def Remove_membership(self, sport):
-        phone = self.Menu("Main Menu > Manage Sports > {} > Remove Membership".format(sport), "Phone to Remove: ")
+        phone = int(self.Menu("Main Menu > Manage Sports > {} > Remove Membership".format(sport), "Phone to Remove: "))
         if phone not in self.Fr.members.keys():
             print("Unknown Member")
             input()
@@ -121,3 +128,28 @@ class UI():
             except:
                 print("Incorrect DoB")
         self.Fr.add_member(phone, name, email, DoB)
+    
+    def Remove_member(self):
+        phone = int(self.Menu("", "Phone to Remove: "))
+        self.Fr.remove_member(phone)
+    
+    def Member_list(self):
+        command = ""
+        while command.lower() != "q":
+            command = self.Menu("Main Menu > Manage Members > Member List", ORDER)
+            if command == "1":
+                self.Menu("Main Menu > Manage Members > Member List > By Name", self.Fr.members_by_name())
+            elif command == "2":
+                self.Menu("Main Menu > Manage Members > Member List > By Phone", self.Fr.members_by_phone())
+            elif command == "3":
+                self.Menu("Main Menu > Manage Members > Member List > By Email", self.Fr.members_by_email())
+            elif command == "4":
+                self.Menu("Main Menu > Manage Members > Member List > By Date of Birth", self.Fr.members_by_DoB())
+            
+    def View_member(self):
+        phone = int(self.Menu("Main Menu > Manage Members > View Member", "Phone to view: "))
+        if phone not in self.Fr.members.keys():
+            print("Unknown Member")
+            input()
+            return
+        self.Menu("Main Menu > Manage Members > View Member > {}".format(phone), self.Fr.get_member(phone))
