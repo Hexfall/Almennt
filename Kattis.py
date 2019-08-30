8f7d6908353f7d736ed049593266368f3ce1b00f
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-08-30 15:15:02 +0000

    Auto-commit

--- Kattis.py
+++ Kattis.py
@@ -1,14 +1,28 @@
-size, height, width = [int(i) for i in input().split()]
-rows = []
-for i in range(height):
-    rows.append([i for i in input().split()])
+def right_shift(shift_str):
+    return shift_str[1:] + shift_str[0]
 
-order = []
-for i in range(size):
-    order.append(input())
-
-for i in range(height):
-    if order[width*i] == rows[i][0]:
-        print('left')
+string = input()
+solution = len(string)
+unq_letters = len(set(string))
+if unq_letters == 1 or unq_letters == len(string):
+    print(unq_letters)
+    exit()
+letter_count = {}
+for letter in string:
+    if letter in letter_count.keys():
+        letter_count[letter] += 1
     else:
-        print('right')
+        letter_count[letter] = 1
+min_count = min(letter_count.values())
+for key in letter_count.keys():
+    letter_count[key] = letter_count[key] // min_count
+cycle_factor = sum(letter_count.values())
+possible_ranges = [i for i in range(cycle_factor, len(string), cycle_factor) if len(string) % i == 0]
+for cycle in possible_ranges:
+    segments = [string[i:i + cycle] for i in range(0, len(string), cycle)]
+    for index in range(1, len(segments)):
+        if segments[index] != right_shift(segments[index - 1]):
+            continue
+    solution = cycle
+    break
+print(solution)
