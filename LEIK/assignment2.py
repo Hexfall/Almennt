0931667d92224c8cbe72d177cbbf19cf36987154
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2019-09-16 10:20:20 +0000

    Auto-commit

--- LEIK/assignment2.py
+++ LEIK/assignment2.py
@@ -1,3 +1,9 @@
+def set_xor(input):
+    num = 0
+    for i in input:
+        num = num ^ i
+    return num
+
 def win_nim(position):
     '''
     Teach a computer how to play Nim perfectly. If the computer starts at a type N
@@ -7,7 +13,25 @@ def win_nim(position):
     non-negative integers corresponding to a type P position.
     If the input is a type P position the program should output -1.
     '''
-    return tuple()
+    
+    def largest_log2(num):
+        if num == 1:
+            return 0
+        num = num >> 1
+        return 1 + largest_log2(num)
+
+    position = list(position)
+
+    if set_xor(position) == 0:
+        return -1
+    largestSingle = 2**largest_log2(set_xor(position))
+    index = 0
+    while True:
+        if largestSingle & position[index]:
+            break
+        index += 1
+    position[index] = set_xor(position[0:index] + position[index + 1:])
+    return tuple(position)
 
 def pick_up_bricks_nimber(position):
     '''
@@ -27,13 +51,11 @@ def pick_up_bricks_nimber_sum(position):
         Run: pick_up_bricks_nimber_sum(position)
         Output: 2
     '''
-    equivalents = 
-    for i in position:
-
-    return -1
+    equivalents = [pick_up_bricks_nimber(i) for i in position]
+    return set_xor(equivalents)
 
 if __name__ == "__main__":
     # You can test things here
-    print(win_nim((3,4,5)))
+    print(win_nim((1315, 316, 1934, 1379, 1412, 408, 3326)))
     print(pick_up_bricks_nimber(7))
     print(pick_up_bricks_nimber_sum((7,3,4)))
